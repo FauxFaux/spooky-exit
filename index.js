6b85c4effc7add19b760252d05eee5c89aa7c00e
Chris West (Faux) <git@goeswhere.com>
2019-09-30 15:46:26 +0100

    promise first

--- index.js
+++ index.js
@@ -13,42 +13,50 @@ async function callGit(cwd, command, idx) {
 
   console.log(idx, '2s');
 
+  const exit = new Promise((resolve, reject) => {
+    console.log(idx, '3p');
+
+    child.once('exit', (code) => {
+      console.log(idx, '7x');
+
+      if (0 === code) {
+        resolve();
+      } else {
+        reject(`exit code: ${code} running ${command} in ${cwd}`);
+      }
+    });
+    child.once('error', (err) => {
+      console.log(idx, '7r');
+      reject(err);
+    });
+
+    console.log(idx, "4d");
+  });
+
+  console.log(idx, "5b");
+
   let buf = '';
   for await (const chunk of child.stdout) {
     const text = chunk.toString('utf-8');
     buf += text;
   }
 
-  console.log(idx, '3c');
+  console.log(idx, '6c');
 
-  return new Promise((resolve, reject) => {
-    console.log(idx, '4p');
+  await exit;
 
-    child.once('exit', (code) => {
-      console.log(idx, '6x');
+  console.log(idx, '8a');
 
-      if (0 === code) {
-        resolve(buf);
-      } else {
-        reject(`exit code: ${code} running ${command} in ${cwd}`);
-      }
-    });
-    child.once('error', (err) => {
-      console.log(idx, '6r');
-      reject(err);
-    });
-
-    console.log(idx, "5d");
-  });
+  return buf;
 }
 
 async function main() {
   const jobs = [];
-  for (let i = 0; i < 100; ++i) {
+  for (let i = 0; i < 24; ++i) {
     jobs.push(['ls-files']);
   }
 
-  await pMap(jobs, (job, idx) => callGit(".", job, idx), {concurrency: 4});
+  await pMap(jobs, (job, idx) => callGit(".", job, idx), {concurrency: 12});
 }
 
 main()
